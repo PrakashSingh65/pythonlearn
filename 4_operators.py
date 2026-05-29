8ac7f281515ac5b2afe45998fd6a067c76535e8e
PrakashSingh65 <pratapsing5656@gmail.com>
2026-05-29 23:06:42 +0530

    Add logical operators examples and create new type.py file

--- 4_operators.py
+++ 4_operators.py
@@ -29,3 +29,24 @@ print (b)
 
 d= 55>88
 print(d)
+
+
+
+# Logical  Operaters
+e = True or False
+
+# True table of 'or'
+
+print("True or False is",True or False)
+print("True or True is",True or True)
+print("False or False is",False or False)
+print("Falsee or True is",False or True)
+
+
+# True taable of 'and'
+print("True and False is",True and False)
+print("True and True is",True and True)
+print("False and False is",False and False)
+print("Falsee and True is",False and True)
+
+print(not(False))
