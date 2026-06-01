213cd5d7284075cc1fca30a09a3378b12ec9117a
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-01 23:44:41 +0530

    Add string manipulation examples in 3_string_function.py

---
+++ chapter 3/3_string_function.py
@@ -0,0 +1,9 @@
+name= "prakash"
+
+print(len(name))
+print(name.endswith("kash"))
+print(name.endswith("pra"))
+print(name.startswith("sh"))
+print(name.capitalize() ) 
+print(name.upper())
+print(name.strip() )
