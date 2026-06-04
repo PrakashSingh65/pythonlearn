c6d207b673cc640d731cce86fff070aa7b82a2b7
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-04 23:03:16 +0530

    Fix formatting and update date in letter template in 2_problem.py

--- chapter 3/2_problem.py
+++ chapter 3/2_problem.py
@@ -1,7 +1,7 @@
 # Wrinte program to fill  in a letter template givenbelow with name and date
 
 letter =''' Dear <|name|>,
-            You are selected !
-            <|Date|>'''
+You are selected !
+<|Date|>'''
 
-print(letter.replace("<|name|>","prakash").replace("<|date|" , "24 september 2050"))
+print(letter.replace("<|name|>","prakash").replace("<|Date|>","24 september 2026"))
