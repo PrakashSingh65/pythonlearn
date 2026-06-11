33fd4cde50f093794f42e8855d82d9ed0967b1ec
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-11 23:07:16 +0530

    Comment out pop demonstration and ensure removal of element in list example in 2_list_methods.py

--- chapter 3/chapter 4/2_list_methods.py
+++ chapter 3/chapter 4/2_list_methods.py
@@ -9,5 +9,6 @@ l1 =[1,25,65,4,6,33]
 # l1.sort()
 # l1.reverse()
 # l1.insert(5,55555)  #insert 55555 such that its index in the list is 5
-print(l1.pop(4))
+# print(l1.pop(4))
+l1.remove(25)
 print(l1)
