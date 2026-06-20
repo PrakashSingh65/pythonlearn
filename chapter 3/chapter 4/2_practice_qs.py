40f3df1237c605409091650a32d9bd7ad3bcd7a7
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-20 23:56:39 +0530

    Implement functionality to accept and display marks of 4 students

--- chapter 3/chapter 4/2_practice_qs.py
+++ chapter 3/chapter 4/2_practice_qs.py
@@ -1,3 +1,15 @@
 #weite a praogram to accept marks of 6 students and display them in a sorted manner.
 
 
+marks = []
+
+f1 = input("enter marks: ")
+marks.append(f1)
+f2 = input("enter marks: ")
+marks.append(f2)
+f3 = input("enter marks: ")
+marks.append(f3)
+f4 = input("enter marks: ")
+marks.append(f4)
+
+print(marks)
