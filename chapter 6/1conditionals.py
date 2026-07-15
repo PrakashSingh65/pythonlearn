6b2cec9873516bb23d5fa5e705e1b6dddc4ee409
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-15 23:38:55 +0530

    Add age validation checks for negative and zero input in age verification script

--- chapter 6/1conditionals.py
+++ chapter 6/1conditionals.py
@@ -4,6 +4,12 @@ if(a>=18):
     print("you are above the age of concent")
     print("good for you")
 
+elif(a<0):
+    print("you are entring an invalid age")
+
+elif(a==0):
+    print("you are entring 0 which is not a valide age")
+
 else:
     print("you are below the age of concent")
 
