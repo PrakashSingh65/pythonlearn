a2945eb41b2c2754df244b83b8858ce06fa53c0a
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-16 23:55:54 +0530

    Fix conditional statements for determining the greatest of four numbers

--- chapter 6/1practice.py
+++ chapter 6/1practice.py
@@ -4,8 +4,14 @@ a = int(input("enter number 3: "))
 a = int(input("enter number 4: "))
 a = int(input("enter number 5: "))
 
-if(a1>a2 and a1>a3 and a1>a4):
+elif(a1>a2 and a1>a3 and a1>a4):
     print("Greatest number is a1:", a1)
 
-if(a2>a1 and a2>a3 and a2>a4):
-    print("Greatest number is a2:", a2)    
+elif(a2>a1 and a2>a3 and a2>a4):
+    print("Greatest number is a2:", a2)
+
+elif(a3>a2 and a3>a2 and a3>a4):
+    print("Greatest number is a3:", a3)
+
+elif(a4>a1 and a4>a2 and a3>a4):
+    print("Greatest number is a4:", a4)
