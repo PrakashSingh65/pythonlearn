4577a958ecd29de6120366fda7e365fb30b88f7d
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-19 00:59:40 +0530

    Add username length validation in practice script

---
+++ chapter 6/4practice.py
@@ -0,0 +1,6 @@
+username = input("enter usernamew")
+
+if(len(username)<10):
+    print("your username contains less then 10 characters")
+else:
+    print("your username contains more than or equal to 10 characters")    
