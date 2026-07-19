f94ccc56fe82ffa5c75b9deae8e50255b7292f7b
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-19 23:35:56 +0530

    Add name input and list membership check in practice script

--- chapter 6/5practice.py
+++ chapter 6/5practice.py
@@ -1,2 +1,8 @@
 l = ["prakash" , "sandeep", "nakul"]
 
+name = input("Enter your name: ")
+
+if(name in l):
+    print("your name is in the list")
+else:
+    print("your name is not in the list")    
