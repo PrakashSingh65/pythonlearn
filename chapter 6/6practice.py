94c4f2335dfea3dd3f64926616b769dbd5aed140
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-19 23:41:13 +0530

    Add grade calculation logic based on user marks input in practice script

---
+++ chapter 6/6practice.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter your marks: "))
+
+if(marks<=100 and marks>=90):
+    grade = "Ex"
+elif(marks<= and marks>=):
+    grade = ""  
+elif(marks<= and marks>=):
+    grade = ""    
+elif(marks<= and marks>=):
+    grade = ""    
+elif(marks<= and marks>=):
+    grade = ""               
