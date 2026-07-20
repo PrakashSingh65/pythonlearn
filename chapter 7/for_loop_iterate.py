d37e47e3844aee2f9cdcdb20cb11f81fcd02cfc7
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-20 23:34:19 +0530

    Add spacing and comments for clarity in for_loop_iterate.py

--- chapter 7/for_loop_iterate.py
+++ chapter 7/for_loop_iterate.py
@@ -1,13 +1,15 @@
+# For Loop with Lists
 # l = [ 1,4,6,234,7,789]
-
 # for i in l:
 #     print(i)
 
+
+#For Loop with Tuples
 t = (6, 231 , 56, 134)
 for i in t:
     print(i)    
 
-
+# For Loop with Stings
 s = "prakash"
 for i in s:
     print(i)    
