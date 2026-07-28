d1d0162832350f6635dde508e6c4d1aba2aa4f0d
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-28 23:30:52 +0530

    Refactor avg function to remove redundant input statements and improve readability

--- chapter 8/function.py
+++ chapter 8/function.py
@@ -17,17 +17,10 @@
 
 
 def avg():
-    a = int(input("enter your number: "))
-b = int(input("enter your number: "))
-c = int(input("enter your number: "))
 
-average = (a+b+c)/3
-print(average)
-
-
-a = int(input("enter your number: "))
-b = int(input("enter your number: "))
-c = int(input("enter your number: "))
+      a = int(input("enter your number: "))
+      b = int(input("enter your number: "))
+      c = int(input("enter your number: "))
 
 
 average = (a + b + c)/3
