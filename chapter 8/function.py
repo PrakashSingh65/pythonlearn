176bb9e8d9bc0d23aec6685de55e4d58e099d3d6
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-28 23:29:19 +0530

    Remove commented-out code and define avg function for average calculation in function.py

--- chapter 8/function.py
+++ chapter 8/function.py
@@ -1,4 +1,23 @@
-a = int(input("enter your number: "))
+# a = int(input("enter your number: "))
+# b = int(input("enter your number: "))
+# c = int(input("enter your number: "))
+
+# average = (a+b+c)/3
+# print(average)
+
+
+# a = int(input("enter your number: "))
+# b = int(input("enter your number: "))
+# c = int(input("enter your number: "))
+
+
+# average = (a + b + c)/3
+# print(average)
+
+
+
+def avg():
+    a = int(input("enter your number: "))
 b = int(input("enter your number: "))
 c = int(input("enter your number: "))
 
